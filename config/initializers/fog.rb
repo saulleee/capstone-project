1a2531bab448c53093d023b75f8dcc9e38154268
saulleee <26151666+saulleee@users.noreply.github.com>
2021-11-20 22:19:15 -0500

    updated fog

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -4,7 +4,6 @@ CarrierWave.configure do |config|
       provider:              "AWS",
       aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      use_iam_profile:       true,
       region:                ENV["S3_REGION"]
     }
     if Rails.env.production?
