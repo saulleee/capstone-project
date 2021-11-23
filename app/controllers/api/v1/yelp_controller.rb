21d387997440bb07412fb08ad62c639f5bc4adec
saulleee <26151666+saulleee@users.noreply.github.com>
2021-11-23 11:21:07 -0500

    updated api response to be LIMIT:3, TERM:cafe

--- app/controllers/api/v1/yelp_controller.rb
+++ app/controllers/api/v1/yelp_controller.rb
@@ -3,22 +3,6 @@ class Api::V1::YelpController < ApplicationController
 
   def search
     response = YelpSearch.retrieve_results(params[:location])
-    render json: response
+    render json: response.trips
   end
 end
-
-
-
-# trying to check the following returns in tests?
-
-# JSON.parse(response.body)["businesses"].size
-# 5
-
-# response.status
-# 200
-
-# Processing by Api::V1::YelpController#search as JSON
-#   Parameters: {"location"=>"boston", "yelp"=>{"location"=>"boston"}}
-# :::::::::::::::::::::::::::::::::::::::::
-# {"location"=>"boston", "controller"=>"api/v1/yelp", "action"=>"search", "yelp"=>{"location"=>"boston"}}
-# :::::::::::::::::::::::::::::::::::::::::
--- app/models/yelp_search.rb
+++ app/models/yelp_search.rb
@@ -2,16 +2,18 @@ class YelpSearch
   API_KEY = ENV["YELP_KEY"]
   API_HOST = "https://api.yelp.com"
   SEARCH_PATH = "/v3/businesses/search"
-  LIMIT = 5
+  LIMIT = 3
+
+  attr_reader :trips
+
+  def initialize(trips)
+    @trips = trips
+  end
 
   def self.retrieve_results(location)
-    cafe_response = yelp_request("cafe", location)
-    parsed_cafe_response = parse_request(cafe_response)
-    
-    bar_response = yelp_request("bar", location)
-    parsed_bar_response = parse_request(bar_response)
-
-    trips = trips_array(parsed_cafe_response, parsed_bar_response)
+    yelp_response = yelp_request("cafe", location)
+    parsed_response = parse_request(yelp_response)
+    YelpSearch.new(parsed_response)
   end
 
   def self.yelp_request(term, location)    
@@ -27,16 +29,5 @@ class YelpSearch
   def self.parse_request(response)
     businesses = JSON.parse(response.body)
     return businesses["businesses"]
-  end
-
-  def self.trips_array(parsed_response_a, parsed_response_b)
-    trips_array = parsed_response_a.map do |place|
-      i = parsed_response_a.find_index(place)
-      subarr = []
-      subarr << place["name"]
-      subarr << parsed_response_b[i]["name"]
-    end
-
-    trips_array
   end
 end
