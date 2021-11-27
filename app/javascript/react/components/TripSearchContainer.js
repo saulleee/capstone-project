751fe53daafdbd09177884308351ebff33276bd1
saulleee <26151666+saulleee@users.noreply.github.com>
2021-11-26 22:58:22 -0500

    created search terms and update yelp_search

--- app/javascript/react/components/TripSearchContainer.js
+++ app/javascript/react/components/TripSearchContainer.js
@@ -1,28 +1,68 @@
 import React, { useState } from "react";
 
+const terms = [
+  { term: "breakfast" },
+  { term: "brunch" },
+  { term: "lunch" },
+  { term: "cafe" },
+  { term: "attractions" },
+  { term: "dinner" },
+  { term: "dessert" },
+  { term: "bar" },
+  { term: "nightlife" }
+];
+
 const TripSearchContainer = (props) => {
   const [location, setLocation] = useState('');
+  const [isChecked, setIsChecked] = useState(
+    new Array(terms.length).fill(false)
+  );
 
-  const handleChange = (event) => {
-    const newLocation = event.target.value;
-    setLocation(newLocation);
+  const handleOnChangeCheck = (position) => {
+    const updatedIsChecked = isChecked.map((item, index) =>
+      index === position ? !item : item
+    );
+
+    setIsChecked(updatedIsChecked);
   }
 
   const handleSubmit = (event) => {
     event.preventDefault();
     let submissionPost = {
-      location: location
+      location: location,
+      terms: isChecked
     }
     props.newSearch(submissionPost);
-    setLocation(location);
   }
+
+  const checkList = terms.map((term, index) => {
+    return (
+      <li key={index}>
+        <div>
+          <input
+            type="checkbox"
+            id={`custom-checkbox-${index}`}
+            name={term}
+            value={term}
+            checked={isChecked[index]}
+            onChange={() => handleOnChangeCheck(index)}
+          />
+          <label htmlFor={`custom-checkbox-${index}`}>{term.term}</label>
+        </div>
+      </li>
+    );
+  });
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <label>Search</label>
-        <input type='text' name='location' value={location} onChange={handleChange} />
-    
+        <input type='text' name='location' placeholder="Boston" value={location} onChange={(e)=>setLocation(e.target.value)} />
+
+        <div>
+          {checkList}
+        </div>
+        
         <input type='submit' value='Submit' />
       </form>
     </div>
