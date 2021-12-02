7f53f7f9ac851851a3de8412c230a2db4dc17c83
saulleee <26151666+saulleee@users.noreply.github.com>
2021-12-02 15:34:51 -0500

    wip persisting state of location

--- app/javascript/react/components/TripSearchContainer.js
+++ app/javascript/react/components/TripSearchContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { terms } from "./utilities/terms";
 import { Checklist } from "./Checklist";
 import SearchBar from "./SearchBar";
